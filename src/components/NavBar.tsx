0a36b83eeec377957fb7a549a9566ad1d80abb3c
Jenesix <acearcadia789@gmail.com>
2024-04-28 23:44:02 +0700

    edit navbar to can responsive in mini phone

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,15 +5,21 @@ import logo from '../../public/Logo.svg';
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center shadow-xl">
-      <div className="mx-2 md:mx-12 flex items-center space-x-2">
-      <Image className='mr-2 md:mr-5' src={logo} width={60} height={60} alt='Learno+'/> 
-        <span className="text-2xl sm:text-3xl font-bold text-primary">LEARNO+</span>
+      <div className="mx-0 md:mx-12 flex items-center space-x-2">
+        <Image className='md:mr-5' src={logo} width={60} height={60} alt='Learno+'/>
+        <span className="text-xl md:text-2xl font-bold text-primary">LEARNO+</span>
       </div>
-      <div className='mx-2 md:mx-10 flex space-x-4'>
-      <button className="bg-primary-light transition-all duration-300 transform hover:scale-105 text-white md:ml-4 xl:ml-6 text-sm lg:text-base font-bold whitespace-nowrap rounded-3xl px-4 lg:px-8 h-12 ">
-          Login
+      <div className='ml-3 sm:ml-0 mx-2 md:mx-10 flex space-x-2 sm:space-x-4'>
+        <button className="
+          bg-primary-light transition-all duration-300 transform hover:scale-105 
+          text-white ml-16 md:ml-4 xl:ml-6 text-sm lg:text-base font-bold 
+          whitespace-nowrap rounded-3xl px-4 sm:px-8 h-12">
+          Log in
         </button>
-        <button className="bg-primary-light transition-all duration-300 transform hover:scale-105 text-white md:ml-4 xl:ml-6 text-sm lg:text-base font-bold whitespace-nowrap rounded-3xl px-4 lg:px-8 h-12">
+        <button className="
+          bg-primary-light transition-all duration-300 transform hover:scale-105 
+          text-white md:ml-4 xl:ml-6 text-sm lg:text-base font-bold 
+          whitespace-nowrap rounded-3xl px-4 sm:px-8 h-12">
           Sign Up
         </button>
       </div>
