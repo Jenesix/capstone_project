93c92b71a765371c2cb815c634f1b62e817e6381
Jenesix <acearcadia789@gmail.com>
2024-05-07 00:22:49 +0700

    fix border select

--- front-end/src/components/Home/HomeCard.tsx
+++ front-end/src/components/Home/HomeCard.tsx
@@ -7,7 +7,7 @@ const Homepage: React.FC = () => {
       <div className="flex flex-col md:flex-row justify-between items-center mt-16 mx-16 2xl:mx-36">
         <h1 className="text-primary font-bold text-2xl mb-4 lg:mb-0 lg:mr-4 whitespace-nowrap">Your Class</h1>
         <div className="w-full md:w-96">
-          <select className="border border-primary rounded-lg w-full h-12 px-2 select-none">
+          <select className="border-2 border-primary rounded-lg w-full h-12 px-2 select-none text-primary font-bold">
             <option value="1">2024/2</option>
             <option value="2">2024/1</option>
             <option value="3">2023/2</option>
