82f1bbf2b53bfadf4d050b1ca31f0818fa1a6f2b
Jenesix <acearcadia789@gmail.com>
2024-05-19 02:19:45 +0700

    Syllabus pdf born!

---
+++ front-end/src/app/id/Syllabus/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+import PdfViewer from '@/components/Syllabus/PdfViewer';
+import React from 'react';
+
+
+const syllabus = [
+    { subjectID: 'CSS234', pdffile: '/pdftest.pdf' }
+];
+
+const Home: React.FC = () => {
+    const currentSyllabus = syllabus[0];
+
+    return (
+        <div className="container mx-auto p-4">
+            <PdfViewer file={currentSyllabus.pdffile} fileName={`${currentSyllabus.subjectID} Syllabus`} />
+        </div>
+    );
+};
+
+export default Home;
