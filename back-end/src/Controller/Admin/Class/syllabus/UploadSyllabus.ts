05dbe30b72dd051c98d6cb7340cb05781c086ccf
Ariez <kritamet.cho@gmail.com>
2024-05-18 19:58:21 +0700

    get/post/delete syllabus :white_check_mark:

---
+++ back-end/src/Controller/Admin/Class/syllabus/UploadSyllabus.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { ClassModel, SyllabusModel } from "../../../../Model/Schema";
+import { uploadSyllabus } from "../../../../utils/UploadFile";
+
+export const UploadSyllabus = async (req: Request, res: Response) => {
+    try {
+        const { classID } = req.params;
+        const file = req.file;
+
+        const findClass = await ClassModel.findById(classID);
+        if (!findClass) {
+            return res.status(400).json({ message: "Class not found" });
+        }
+
+        let fileUrl = "";
+        if (file) {
+            fileUrl = await uploadSyllabus(file);
+        }
+
+        const syllabus = new SyllabusModel({
+            file_syl: fileUrl,
+            ClassID: classID
+        });
+        await syllabus.save();
+
+        return res.status(200).json(syllabus);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
