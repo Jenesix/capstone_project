16875233bcafb5ac8a13e4bce3b6631f4a35aa02
Ariez <kritamet.cho@gmail.com>
2024-05-20 15:23:14 +0700

    handle upload file folder

--- back-end/src/Controller/User/resource/UploadResource.ts
+++ back-end/src/Controller/User/resource/UploadResource.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
-import { ResourceModel, ClassModel } from "../../../Model/Schema";
+import { ResourceModel, ClassModel, ResourceFolderModel } from "../../../Model/Schema";
 import { uploadResourceFile } from "../../../utils/UploadFile";
 
 export const UploadResource = async (req: Request, res: Response) => {
     try {
-        const { classID } = req.params;
+        const { classID, folderID } = req.query;
         const file = req.file;
 
         // let fileUrl = "";
@@ -12,18 +12,32 @@ export const UploadResource = async (req: Request, res: Response) => {
         //     fileUrl = await uploadFile(file);
         // }
 
+        const findClass = await ClassModel.findById(classID);
+        if (!findClass) {
+            return res.status(400).json({ message: "Class not found" });
+        }
+
         if (!file) {
             return res.status(400).json({ message: "Please upload a file" });
         }
         const fileUrl = await uploadResourceFile(file);
 
-        const resource = new ResourceModel({
-            file_rs: fileUrl,
-            ClassID: classID,
-        });
-        await resource.save();
-
-        return res.status(200).json({ message: "Upload resource success", resource });
+        if (!folderID) {
+            const resource = new ResourceModel({
+                file_rs: fileUrl,
+                ClassID: classID,
+            });
+            await resource.save();
+            return res.status(200).json({ message: "Upload resource success", resource });
+        } else {
+            const resource = new ResourceModel({
+                file_rs: fileUrl,
+                ClassID: classID,
+                ResourceFolderID: folderID
+            });
+            await resource.save();
+            return res.status(200).json({ message: "Upload resource success", resource });
+        }  
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
