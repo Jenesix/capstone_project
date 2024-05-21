4160b2b033bded11f4edd545b6f9d56ceb9ea746
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2024-05-21 11:12:39 +0700

    QnABoard part.1

--- front-end/src/components/QnABoard/QnABoardPage.tsx
+++ front-end/src/components/QnABoard/QnABoardPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import profile from '../../../public/profile.svg';
 import UserCard from './UserCard';
+import QnACard from './QnACard';
 
 // ข้อมูลหลังบ้าน
 const User = [
@@ -33,19 +34,28 @@ const countUser = User.length
 const QnABoardPage: React.FC = () => {
     return (
         <div className="min-h-screen flex flex-col mt-12 w-full px-4 sm:px-8 pb-6">
-            <h1 className="text-primary text-center font-bold text-xl sm:text-2xl lg:text-3xl">Q&Aa Board</h1>
+            <h1 className="text-primary text-center font-bold text-xl sm:text-2xl lg:text-3xl">Q&A Board</h1>
             <div className=' justify-center mt-12 2xl:mx-20'>
             <h2 className="text-salate-1000 text-center font-bold text-2xl max-w-48 mb-5">{countUser} People</h2>
+            <div className='grid grid-cols-2'>
+            <QnACard/>
+            <QnACard/>
+            <QnACard/>
+            <QnACard/>
+            </div>
+            {/* <QnACard
+            sizecard=""
+            /> */}
                         {User.map(user => (
                                     <UserCard
                                         profileImage={user.profileImage}
                                         user_id={user.user_id}
                                         firstname={user.firstname}
                                         lastname={user.lastname}
-                                        sizeprofile='min-w-20 min-h-20'
+                                        sizeprofile='min-w-12 min-h-12'
                                         sizedivtext='min-w-96'
                                         sizenameuser='text-base'
-                                        sizeiduser='text-sm'
+                                        sizeiduser='text-xs'
                                     />
                         ))}
             </div>
