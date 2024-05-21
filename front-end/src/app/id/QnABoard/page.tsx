f8512f588967f4443608b458dc59827d61f2e541
JaTingJA <55779319+nutgzazah@users.noreply.github.com>
2024-05-21 09:18:34 +0700

    Change path QnA & Add UserCard Component
    Q&A Board ตอนนี้ใช้ path "/id/QnABoard"
    เพิ่ม UserCard component ที่สามารถปรับใช้ได้เลย ไม่ต้องสร้างใหม่หลายๆรอบโดยใส่ค่าได้ตัวอย่าง
    <UserCard
                                            profileImage={user.profileImage}
                                            user_id={user.user_id}
                                            firstname={user.firstname}
                                            lastname={user.lastname}
                                            sizeprofile='min-w-20 min-h-20'
                                            sizedivtext='min-w-96'
                                            sizenameuser='text-base'
                                            sizeiduser='text-sm'
                                        />

---
+++ front-end/src/app/id/QnABoard/page.tsx
@@ -0,0 +1,10 @@
+import QnABoardPage from "@/components/QnABoard/QnABoardPage";
+
+
+export default function Home() {
+    return (
+        <>
+            <QnABoardPage />
+        </>
+    );
+}
---
+++ front-end/src/components/QnABoard/QnABoardPage.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import profile from '../../../public/profile.svg';
+import UserCard from './UserCard';
+
+// ข้อมูลหลังบ้าน
+const User = [
+    {
+        user_id: "65090500414",
+        email: "nutnutnut@kmutt.ac.th",
+        firstname: "Natthapon",
+        lastname: "Tanateeraanan",
+        profileImage: profile,
+        FacultyID: "Faculty of Science",
+        DepartmentID: "Department of Computer Science",
+        MajorID: "Computer Science",
+    },
+    {
+        user_id: "65090500415",
+        email: "godsung@kmutt.ac.th",
+        firstname: "Wutthawat",
+        lastname: "Saart",
+        profileImage: profile,
+        FacultyID: "Faculty of Architecture",
+        DepartmentID: "Department of Architectural Design",
+        MajorID: "Architecture",
+    },
+    
+];
+// นับจำนวน User
+const countUser = User.length
+
+
+const QnABoardPage: React.FC = () => {
+    return (
+        <div className="min-h-screen flex flex-col mt-12 w-full px-4 sm:px-8 pb-6">
+            <h1 className="text-primary text-center font-bold text-xl sm:text-2xl lg:text-3xl">Q&Aa Board</h1>
+            <div className=' justify-center mt-12 2xl:mx-20'>
+            <h2 className="text-salate-1000 text-center font-bold text-2xl max-w-48 mb-5">{countUser} People</h2>
+                        {User.map(user => (
+                                    <UserCard
+                                        profileImage={user.profileImage}
+                                        user_id={user.user_id}
+                                        firstname={user.firstname}
+                                        lastname={user.lastname}
+                                        sizeprofile='min-w-20 min-h-20'
+                                        sizedivtext='min-w-96'
+                                        sizenameuser='text-base'
+                                        sizeiduser='text-sm'
+                                    />
+                        ))}
+            </div>
+        </div>
+        
+    );
+};
+
+export default QnABoardPage;
