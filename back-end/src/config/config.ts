d73bfc25d1a067004d164b81db998e79a4487a76
Ariez <kritamet.cho@gmail.com>
2024-05-23 10:19:38 +0700

    Update config.ts

--- back-end/src/config/config.ts
+++ back-end/src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 8000;
+export const PORT = Number(process.env.PORT) || 8000;
 export const MONGO_URI = String(process.env.MONGO_URI);
 export const secret_jwt = process.env.SECRET_JWT;
 export const supabaseUrl = String(process.env.SUPABASE_URL);
