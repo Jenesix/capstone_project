40bab78ce791b3802693693c7da006dc48df65d0
Ariez <kritamet.cho@gmail.com>
2024-05-24 21:15:17 +0700

    Update GetAnnounce.ts

--- back-end/src/Controller/User/announcement/GetAnnounce.ts
+++ back-end/src/Controller/User/announcement/GetAnnounce.ts
@@ -7,7 +7,9 @@ export const GetAnnounce = async (req: Request, res: Response) => {
         if (!classID) {
             return res.status(400).json({ message: "Class not found" });
         }
-        const announcement = await AnnouncementModel.find({ ClassID: classID });
+        const announcement = await AnnouncementModel.find({ ClassID: classID })
+            .populate({ path: "UserID" });
+
         return res.status(200).json(announcement);
     } catch (error) {
         console.log(error);
