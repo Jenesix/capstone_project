09f167be29df3c8768851eb180ffee699fe1aa65
Jenesix <acearcadia789@gmail.com>
2024-05-25 04:04:09 +0700

    Assignmentpage connect

--- front-end/src/components/Assignment/AssignmentCard.tsx
+++ front-end/src/components/Assignment/AssignmentCard.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import { FaBookmark } from 'react-icons/fa';
 
 interface AssignmentCardProps {
+    classID: string;
+    assignID: string;
     title: string;
     description: string;
     dueDate: string;
     status: string;
 }
 
-const AssignmentCard: React.FC<AssignmentCardProps> = ({ title, description, dueDate, status }) => {
+const AssignmentCard: React.FC<AssignmentCardProps> = ({ classID, assignID, title, description, dueDate, status }) => {
     return (
-        <Link href="Assignment/id">
+        <Link href={`/${classID}/Assignment/${assignID}`}>
             <div className="bg-white shadow-2xl rounded-lg p-4 mb-4 flex flex-row gap-3">
                 <div>
-                    <FaBookmark className={` top-2 left-2 text-4xl ${status === 'To Do' ? 'text-bookmark1' : status === 'Submitted' ? 'text-bookmark2' : 'text-bookmark3'}`} />
+                    <FaBookmark className={`top-2 left-2 text-4xl ${status === 'To Do' ? 'text-bookmark1' : status === 'Submitted' ? 'text-bookmark2' : 'text-bookmark3'}`} />
                 </div>
                 <div>
                     <div className="flex justify-between items-center mb-2">
