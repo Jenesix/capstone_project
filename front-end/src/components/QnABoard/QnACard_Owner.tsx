c4eb57303e9c731a90d67ff48d58bd7ca8cbe18c
Jenesix <acearcadia789@gmail.com>
2024-05-24 21:49:36 +0700

    QnA responsive

--- front-end/src/components/QnABoard/QnACard_Owner.tsx
+++ front-end/src/components/QnABoard/QnACard_Owner.tsx
@@ -33,20 +33,20 @@ const QnACard: React.FC<QnACardProps> = ({
     };
 
     return (
-        <div className={`bg-content-light shadow-2xl rounded-lg p-4 pl-12 pt-8 mb-8 mr-8 ${size_card} text-salate-1000 overflow-hidden `}>
-            <div className='flex flex-row mb-4'>
-                <h1 className='font-bold text-xl text-primary truncate pr-4 w-full'>{board_title}</h1>
+        <div className={`bg-content-light shadow-2xl rounded-lg p-4 mb-8 ${size_card} text-salate-1000 overflow-hidden`}>
+            <div className="flex flex-row mb-4">
+                <h1 className="font-bold text-xl text-primary truncate pr-4 flex-grow">{board_title}</h1>
                 <div className="relative">
-                    <SlOptionsVertical className='ml-auto cursor-pointer' onClick={toggleDropdown} />
+                    <SlOptionsVertical className="ml-auto cursor-pointer" onClick={toggleDropdown} />
                     {dropdownVisible && (
                         <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
-                            <Link href="/classID/QnABoard/qnaboardID/Edit">
-                                <p className=" w-full text-left px-4 py-2 hover:bg-content-light">
-                                Edit
+                            <Link href={`/classID/QnABoard/qnaboardID/Edit`}>
+                                <p className="w-full text-left px-4 py-2 hover:bg-content-light">
+                                    Edit
                                 </p>
                             </Link>
                             <button
-                                className=" w-full text-left px-4 py-2 hover:bg-content-light"
+                                className="w-full text-left px-4 py-2 hover:bg-content-light"
                                 onClick={() => console.log('Delete clicked')}
                             >
                                 Delete
@@ -60,8 +60,8 @@ const QnACard: React.FC<QnACardProps> = ({
                 <div>
                     <div className={`mb-4 ${height_detail} font-semibold truncate whitespace-normal`}>{board_desc}</div>
 
-                    <div className='flex flex-row min-h-20'>
-                        <div className="flex items-center">
+                    <div className="flex flex-col md:flex-row min-h-20">
+                        <div className="flex items-center mb-4 md:mb-0">
                             <div className={`${size_profile} rounded-full overflow-hidden`}>
                                 <Image
                                     src={profileImage}
@@ -75,15 +75,16 @@ const QnACard: React.FC<QnACardProps> = ({
                             </div>
                         </div>
 
-                        <div className='ml-auto mt-auto flex flex-col items-end'>
+                        <div className="md:ml-auto flex flex-col items-end">
                             <div className={`relative mb-2 ${size_image}`}>
-                                <Image className={size_image}
+                                <Image
                                     src={postimage}
-                                    layout='fill'
+                                    layout="fill"
+                                    objectFit="cover"
                                     alt="postimage"
                                 />
                             </div>
-                            <div className='font-base'>{time}</div>
+                            <div className="font-base">{time}</div>
                         </div>
                     </div>
                 </div>
