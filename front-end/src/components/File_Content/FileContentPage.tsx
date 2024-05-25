c4f4f891f3cd86e43ba0ada49ceaf6a28a06ada9
Ariez <kritamet.cho@gmail.com>
2024-05-25 22:59:56 +0700

    fetch resource & folder

--- front-end/src/components/File_Content/FileContentPage.tsx
+++ front-end/src/components/File_Content/FileContentPage.tsx
@@ -1,16 +1,21 @@
+"use client";
 import { FC } from 'react';
 import { FaFolder, FaFile } from "react-icons/fa";
-
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
 
-const files = [
+import { axioslib } from '@/lib/axioslib';
+import { Resource, ResourceFolder } from '@/interface/interface';
+
+const filesmock = [
     { name: 'Slide_Week1.pdf', type: 'pdf' },
     { name: 'Lab_test_result.png', type: 'image' },
     { name: 'App.jsx', type: 'code' },
     { name: 'Test.file', type: 'file' },
 ];
 
-const folders = [
+const foldersmock = [
     'Study Lecture Week 1',
     'Study Lecture Week 2',
     'Test Lab For ALL',
@@ -19,6 +24,35 @@ const folders = [
 ];
 
 const FileContentPage: FC = () => {
+    const [folders, setFolders] = useState<ResourceFolder[]>([]);
+    const [files, setFiles] = useState<Resource[]>([]);
+    const { classID } = useParams();
+
+    const fetchFolder = async () => {
+        try {
+            const response = await axioslib.get(`/api/user/getfolder/${classID}`);
+            setFolders(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchFile = async () => {
+        try {
+            const response = await axioslib.get(`/api/user/getresource/${classID}`);
+            setFiles(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchFolder();
+        fetchFile();
+    }, []);
+
     return (
         <div className="container mx-auto p-6 min-h-screen">
             <h1 className="text-3xl font-bold text-center text-primary mb-6">File & Content</h1>
@@ -27,11 +61,11 @@ const FileContentPage: FC = () => {
                 <h2 className="text-xl font-bold mb-4 text-gray">Folders</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 text-salate-1000 font-bold">
                     {folders.map((folder, index) => (
-                        <Link href='/id/File_Content/Folder' key={index}>
+                        <Link href={`/${classID}/File_Content/${folder._id}`} key={index}>
                             <div className="p-2 rounded shadow flex items-center cursor-pointer">
                                 <div className="flex items-center px-4">
                                     <FaFolder className="mr-2 " />
-                                    <span>{folder}</span>
+                                    <span>{folder.folder_name}</span>
                                 </div>
                             </div>
                         </Link>
@@ -45,14 +79,15 @@ const FileContentPage: FC = () => {
                     {files.map((file, index) => (
                         <div
                             key={index}
-                            className="p-4 rounded shadow flex items-center justify-between "
+                            className="p-4 rounded shadow flex items-center justify-between"
                         >
-                            <div className="flex items-center text-salate-1000">
-                                {file.type === 'pdf' && <FaFile className="mr-2 " />}
-                                {file.type === 'image' && <FaFile className="mr-2 " />}
-                                {file.type === 'code' && <FaFile className="mr-2 " />}
-                                {file.type === 'file' && <FaFile className="mr-2 " />}
-                                <span className="truncate">{file.name}</span>
+                            <div className="flex items-center text-salate-1000 truncate">
+                                <FaFile className="mr-2 " />
+                                <span className="truncate">
+                                    <a href={file.file_rs} target="_blank">
+                                        {file.file_rs.substring(file.file_rs.lastIndexOf('/') + 1)}
+                                    </a>
+                                </span>
                             </div>
                         </div>
                     ))}
