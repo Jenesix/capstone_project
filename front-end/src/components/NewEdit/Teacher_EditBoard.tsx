9e49a2590d6d3f24c882ab01e96b1d8543b1114d
Ariez <kritamet.cho@gmail.com>
2024-05-28 19:41:33 +0700

    update teacher side

--- front-end/src/components/NewEdit/Teacher_EditBoard.tsx
+++ front-end/src/components/NewEdit/Teacher_EditBoard.tsx
@@ -1,17 +1,30 @@
 "use client";
 import { useParams } from 'next/navigation';
 import { FaTrashAlt } from "react-icons/fa";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/legacy/image';
 import Link from "next/link";
 
+import { axioslib } from '../../lib/axioslib';
+import { Post } from '../../interface/interface';
+
 
 const Teacher_EditBoard: React.FC = () => {
-    const { classID } = useParams();
+    const { classID, postID } = useParams();
+
+    const [oldPost, setOldPost] = useState<Post | undefined>(undefined);
+    useEffect(() => {
+        const fetchPost = async () => {
+            const response = await axioslib.get(`/api/user/getpostbyid/${postID}`);
+            setOldPost(response.data);
+            console.log(response.data);
+        };
+        fetchPost();
+    }, []);
 
     const [title, setTitle] = useState<string>('');
     const [description, setDescription] = useState<string>('');
-    const [image, setImage] = useState<File | null>(null);
+    const [image, setImage] = useState<File[] | null>([]);
     const [preview, setPreview] = useState<string | null>(null);
 
     const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,14 +36,15 @@ const Teacher_EditBoard: React.FC = () => {
     };
 
     const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const file = e.target.files?.[0];
-        if (file) {
+        const files = e.target.files;
+        if (files) {
+            const file = Array.from(files);
             setImage(file);
             const reader = new FileReader();
             reader.onloadend = () => {
                 setPreview(reader.result as string);
             };
-            reader.readAsDataURL(file);
+            reader.readAsDataURL(file[0]);
         }
     };
 
@@ -39,19 +53,23 @@ const Teacher_EditBoard: React.FC = () => {
         setPreview(null);
     };
 
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        
-        // Process form data here
+
         const formData = new FormData();
-        formData.append('title', title);
-        formData.append('description', description);
+        formData.append('title_p', title);
+        formData.append('description_p', description);
         if (image) {
-            formData.append('image', image);
+            formData.append('files', image[0]);
         }
 
-        // You can now send formData to your server or API endpoint
-        console.log('Form submitted:', { title, description, image });
+        try {
+            await axioslib.post(`/api/user/editpost/${postID}`, formData).then(() => {
+                window.location.href = `Teacher/${classID}/QnABoard`;
+            });
+        } catch (error) {
+            console.error('Error creating post:', error);
+        }
     };
 
 
@@ -64,64 +82,66 @@ const Teacher_EditBoard: React.FC = () => {
                 </button>
             </Link>
             <div className='flex md:justify-center'>
-                    <div className='w-1/3 flex flex-col text-salate-1000'>
-                        <form onSubmit={handleSubmit}>
+                <div className='w-1/3 flex flex-col text-salate-1000'>
+                    <form onSubmit={handleSubmit}>
                         <div className='w-60 md:w-72 lg:w-96 2xl:w-auto bg-content-light rounded-t-2xl text-center border-b border-salate-1000 pt-6 p-4 md:p-8 lg:p-12 text-sm md:text-xl lg:text-2xl font-extrabold'>
-                                Edit Q&A Board
-                            </div>
-                            <div className='w-60 md:w-72 lg:w-96 2xl:w-auto bg-content-light rounded-b-2xl text-xs md:text-base lg:text-lg pt-4 p-4 md:p-8 lg:p-12'>
-                                <p>Board Title</p>
-                                <input
-                                    value={title}
-                                    onChange={handleTitleChange}
-                                    placeholder="Your Title"
-                                    type='text'
-                                    className='mt-2 p-2 pl-4 bg-white border-salate-1000 rounded-xl w-full'
-                                />
-                                
-                                <p className='mt-4'>Board Description</p>
-                                <textarea
-                                    value={description}
-                                    onChange={handleDescriptionChange}
-                                    placeholder="Your Description"
-                                    className='mt-2 p-2 pl-4 bg-white border-salate-1000 rounded-xl w-full min-h-60'
-                                />
-                                
-                                <p className='mt-4'>Image (Optional)</p>
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={handleImageChange}
-                                    className='mt-2 p-2 bg-white border-salate-1000 rounded-xl w-full'
-                                />
-                                {preview && (
-                                    <div className='mt-4'>
-                                        <Image
-                                            src={preview}
-                                            alt="Preview"
-                                            width={500}
-                                            height={300}
-                                            className='rounded-xl object-contain bg-white'
-                                        />
-                                        <button
-                                            onClick={removeImage}
-                                            type="button"
-                                            className='mt-2 bg-white text-bookmark1 px-4 py-2 rounded-lg flex items-center border'
-                                        >
-                                            <FaTrashAlt className="mr-2" />
-                                            Remove Image
-                                        </button>
-                                    </div>
-                                )}
-                                <button
-                                    type="submit"
-                                    className="text-xs md:text-base bg-bookmark2 text-white font-bold py-4 px-4 md:px-12 lg:px-14 rounded block mx-auto transition-all duration-300 transform hover:scale-105 mt-4"
-                                >
-                                    Submit
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                            Edit Q&A Board
+                        </div>
+                        <div className='w-60 md:w-72 lg:w-96 2xl:w-auto bg-content-light rounded-b-2xl text-xs md:text-base lg:text-lg pt-4 p-4 md:p-8 lg:p-12'>
+                            <p>Board Title</p>
+                            <input
+                                value={oldPost?.title_p}
+                                onChange={handleTitleChange}
+                                placeholder="Your Title"
+                                type='text'
+                                disabled
+                                className='mt-2 italic p-2 pl-4 bg-slate-1000 border-salate-1000 rounded-xl w-full'
+                            />
+
+                            <p className='mt-4'>Board Description</p>
+                            <textarea
+                                value={oldPost?.description_p}
+                                onChange={handleDescriptionChange}
+                                placeholder="Your Description"
+                                className='mt-2 p-2 pl-4 bg-white border-salate-1000 rounded-xl w-full min-h-60'
+                            />
+
+                            <p className='mt-4'>Image (Optional)</p>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImageChange}
+                                className='mt-2 p-2 bg-white border-salate-1000 rounded-xl w-full'
+                                multiple
+                            />
+                            {preview && (
+                                <div className='mt-4'>
+                                    <Image
+                                        src={preview}
+                                        alt="Preview"
+                                        width={500}
+                                        height={300}
+                                        className='rounded-xl object-contain bg-white'
+                                    />
+                                    <button
+                                        onClick={removeImage}
+                                        type="button"
+                                        className='mt-2 bg-white text-bookmark1 px-4 py-2 rounded-lg flex items-center border'
+                                    >
+                                        <FaTrashAlt className="mr-2" />
+                                        Remove Image
+                                    </button>
+                                </div>
+                            )}
+                            <button
+                                type="submit"
+                                className="text-xs md:text-base bg-bookmark2 text-white font-bold py-4 px-4 md:px-12 lg:px-14 rounded block mx-auto transition-all duration-300 transform hover:scale-105 mt-4"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
--- front-end/src/components/QnABoard/Teacher_QnABoardPage.tsx
+++ front-end/src/components/QnABoard/Teacher_QnABoardPage.tsx
@@ -8,74 +8,65 @@ import cristiano from '../../../public/cristiano.jpg';
 import Link from 'next/link';
 import { IoMdAddCircle } from "react-icons/io";
 
-const Post = [
-    {
-        boardID: "id1",
-        user_id: "65090500414",
-        firstname: "Natthapon",
-        lastname: "Tanateeraanan",
-        profileImage: profile,
-        title_p: "Cristiano Ronaldo: An Idol for Excellence and Perseverance in Study and Life",
-        description_p: "Cristiano Ronaldo, renowned for his exceptional talent and relentless dedication in the world of football, serves as an inspiring figure for students striving for success.",
-        post_image: cristiano,
-        time_p: "15 Jan 2024 16:00",
-    },
-    {
-        boardID: "id2",
-        user_id: "65090500415",
-        firstname: "Nagfdhhn",
-        lastname: "Twerewraanan",
-        profileImage: profile,
-        title_p: "is there any way to make 4g(lte) faster than 5g?",
-        description_p: "I know that not many places provide 5g network and the infrase aiming to achieve their dreame aiming to achieve their dreame aiming to achieve their dreame aiming to achieve their dreame aiming to achieve their dreamtructure to provide it at a nation wide scale is extremely costly. Is there any way to speed up 4g network with the infrastructure we already have?",
-        post_image: Announcementtest,
-        time_p: "16 Jan 2024 16:00",
-    },
-    {
-        boardID: "id3",
-        user_id: "65090500416",
-        firstname: "Nadfgson",
-        lastname: "Tanateertyyan",
-        profileImage: profile,
-        title_p: "What is the difference between transport and tunnel mode in IPsec SAs?",
-        description_p: "What is the difference between transport and tunnel mode in IPsec SAs?",
-        post_image: Announcementtest,
-        time_p: "17 Jan 2024 16:00",
-    },
-];
+import { useState, useEffect } from 'react';
+import { axioslib } from '@/lib/axioslib';
+import { Post, User } from '@/interface/interface';
+import { format, parseISO } from 'date-fns';
 
-const countPost = Post.length;
+interface PostWithUser extends Omit<Post, 'UserID'> {
+    UserID: User;
+}
 
 const Teacher_QnABoardPage: React.FC = () => {
     const { classID, postID } = useParams();
+
+    const [posts, setPosts] = useState<PostWithUser[]>([]);
+    useEffect(() => {
+        if (classID) {
+            axioslib.get(`/api/user/getpost/${classID}`)
+                .then(response => {
+                    setPosts(response.data);
+                })
+                .catch(error => {
+                    console.error('Error fetching posts:', error);
+                });
+        }
+    }, [classID]);
+
+    const countPost = posts.length;
+    const formatDate = (date: string) => {
+        const dateObj = parseISO(date);
+        return format(dateObj, 'dd/MM/yyyy, HH:mm');
+    }
+
     return (
         <div className="min-h-screen flex flex-col mt-12 w-full px-4 sm:px-8 pb-6">
             <h1 className="text-primary text-center font-bold text-xl sm:text-2xl lg:text-3xl">Q&A Board</h1>
             <div className="justify-center mt-12 2xl:mx-20">
-                <h2 className="text-salate-1000 text-center font-bold text-2xl max-w-48 mb-5">{countPost} Board</h2>
+                <h2 className="text-salate-1000 text-center font-bold text-2xl max-w-48 mb-5">{countPost} Board{countPost !== 1 ? 's' : ''}</h2>
                 <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
-                    {Post.map(post => (
+                    {posts.map(post => (
                         <QnACard_Owner
-                            key={post.boardID}
-                            boardID={post.boardID}
-                            board_title={post.title_p}
-                            editLink={`/Teacher/${classID}/QnABoard/${postID}/Edit`}
-                            board_desc={post.description_p}
+                            key={post._id}
+                            postID={post._id}
+                            post_title={post.title_p}
+                            editLink={`/Teacher/${classID}/QnABoard/${post._id}/Edit`}
+                            post_desc={post.description_p}
                             postimage={post.post_image}
-                            time={post.time_p}
-                            user_id={post.user_id}
-                            profileImage={post.profileImage}
-                            firstname={post.firstname}
-                            lastname={post.lastname}
+                            time={formatDate(String(post.time_p))}
+                            user_id={post.UserID.user_id}
+                            profileImage={profile}
+                            firstname={post.UserID.firstname}
+                            lastname={post.UserID.lastname}
                         />
                     ))}
                 </div>
             </div>
             <Link href={`/Teacher/${classID}/QnABoard/New`}>
-                    <div className='mb-5 pl-20 pr-20 bottom-0 ml-4 bg-content-light rounded-3xl flex flex-row items-center justify-center p-4 fixed text-salate-1000'>
-                        <IoMdAddCircle className=' size-10 '/>
-                        <p className='pl-2 font-bold text-xl'>New Board</p>
-                    </div>
+                <div className='mb-5 pl-20 pr-20 bottom-0 ml-4 bg-content-light rounded-3xl flex flex-row items-center justify-center p-4 fixed text-salate-1000'>
+                    <IoMdAddCircle className=' size-10 ' />
+                    <p className='pl-2 font-bold text-xl'>New Board</p>
+                </div>
             </Link>
         </div>
     );
