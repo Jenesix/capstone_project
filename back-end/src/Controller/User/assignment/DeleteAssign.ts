345c31c2ed821afd79c76a9e73b62240b0b6e9f4
Ariez <kritamet.cho@gmail.com>
2024-05-28 22:19:46 +0700

    fix FK delete

--- back-end/src/Controller/User/assignment/DeleteAssign.ts
+++ back-end/src/Controller/User/assignment/DeleteAssign.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
-import { AssignmentModel } from "../../../Model/Schema";
+import { AssignmentModel, AssignmentTurninModel } from "../../../Model/Schema";
 
 export const DeleteAssign = async (req: Request, res: Response) => {
     try {
         const { assignID } = req.params;
-        const result = await AssignmentModel.findByIdAndDelete(assignID);
-        if (!result) {
+
+        const assign = await AssignmentModel.findById(assignID);
+        if (!assign) {
             return res.status(400).json({ message: "Assignment not found" });
         }
+
+        await AssignmentTurninModel.deleteMany({ _id: { $in: assign.TurninID } });
+        await assign.deleteOne();
+
         return res.status(200).json({ message: "Delete assignment success" });
     } catch (error) {
         console.log(error);
--- back-end/src/Controller/User/attendance/DeleteAttend.ts
+++ back-end/src/Controller/User/attendance/DeleteAttend.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
-import { AttendanceModel } from "../../../Model/Schema";
+import { AttendanceModel, AttendanceCheckModel } from "../../../Model/Schema";
 
 export const DeleteAttend = async (req: Request, res: Response) => {
     try {
         const { attendID } = req.params;
-        const result = await AttendanceModel.findByIdAndDelete(attendID);
-        if (!result) {
+        
+        const attend = await AttendanceModel.findById(attendID);
+        if (!attend) {
             return res.status(400).json({ message: "Attendance not found" });
         }
+
+        await AttendanceCheckModel.deleteMany({ _id: { $in: attend.CheckID } });
+        await attend.deleteOne();
+
         return res.status(200).json({ message: "Delete attendance success" });
     } catch (error) {
         console.log(error);
